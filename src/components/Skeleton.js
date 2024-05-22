9baae2a4c52573f38416583919ef447b355d4eac
Dan <danporeda@yahoo.com>
2024-05-21 21:12:00 -0400

    added shimmer code for Skeleton animation to tailwind.config.js

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -2,7 +2,9 @@ import classNames from 'classnames';
 
 function Skeleton({ times }) {
   const boxes = Array(times).fill(0).map((_, i) => {
-    return <div key={i} />
+    return <div key={i}>
+      <div />
+    </div>
   });
 
   return boxes;
