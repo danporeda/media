70417a78115c49585b8b9b59a8cadc765c6b3d9e
Dan <danporeda@yahoo.com>
2024-05-26 12:20:29 -0400

    wired up usersListItem with useThunk(removeUser). delete button with each item, with onClick to delete via removeUser

---
+++ src/components/UsersListItem.js
@@ -0,0 +1,26 @@
+import { GoTrashCan } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
+function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  }
+
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoading} onClick={handleClick()}>
+
+        </Button>
+        {error && <div>Error Deleting User.</div>}
+        {user.name}
+      </div>
+    </div>
+  );
+}
+
+export default UsersListItem;
