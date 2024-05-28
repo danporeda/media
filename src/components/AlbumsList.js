818066bdedfe88526934edec9d2e3543f69fefd4
Dan <danporeda@yahoo.com>
2024-05-27 23:54:01 -0400

    created addAlbum endpoint in albumsApi, put its useAddAlbumMutation as the onClick in an Add Album button in the header of the albumsList ExpandablePanel

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,43 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, result] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+
+      return <ExpandablePanel key={album.id} header={header}>
+        List of photos in the album
+      </ExpandablePanel>
+    })
+  }
+
   return (
-    <div>Albums for {user.name}</div>
+    <div>
+      <div className='flex justify-between items-center mb-2'>
+      Albums for {user.name}
+        <Button onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
+      <div>
+        {content}
+      </div>
+    </div>
   );
 }
 
